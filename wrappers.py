f57d32722155215578e21b300747fa28a66d46ce
Aaranya Prasad <aaaaranya@gmail.com>
2022-12-15 03:50:44 +0530

    Fix read

--- wrappers.py
+++ wrappers.py
@@ -6,10 +6,17 @@ from queue import Queue
 
 def write_to_kertish(local_name,fs_name):
     local_name_modified = "local:" + local_name
-    command =["/Users/satchithari/Documents/4-1/DSTN/Project/osx_v22.2.0147_amd64/krtfs","-t",config.kertish_ip,"cp",local_name_modified,fs_name]
+    command =["./krtfs","-t",config.kertish_ip,"cp",local_name_modified,fs_name]
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out = p.stdout.read().decode()
     print(out)
 
 if __name__ == '__main__':
 	write_to_kertish("test2.txt","/test2.txt")
+
+def get_from_kertish(fs_name,local_name):
+    local_name_modified = "local:" + local_name
+    command =["./krtfs","-t",config.kertish_ip,"cp",fs_name,local_name_modified]
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = p.stdout.read().decode()
+    print(out)
